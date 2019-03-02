90dbc267185391f4f16aa7562c3ee1901e0f2f71
adrianolego <adriano__silva__@hotmail.com>
2019-03-02 07:29:18 -0300

    Reorganizando diretórios

--- ProjetoArquitetural/ControleColeta/src/main/java/com/example/ControleColeta/service/CargaService.java
+++
@@ -1,7 +0,0 @@
-package com.example.ControleColeta.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CargaService {
-}
--- ProjetoArquitetural/ControleColeta/src/main/java/com/example/ControleColeta/service/RastreioService.java
+++
@@ -1,4 +0,0 @@
-package com.example.ControleColeta.service;
-
-public class RastreioService {
-}
