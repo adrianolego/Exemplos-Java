2f0bc81e17ae04e7961c38e2ec3db41b5257e37a
adrianolego <adriano__silva__@hotmail.com>
2019-07-13 16:47:44 -0300

    Atualização

---
+++ ModeloConceitual/src/main/java/com/adriano/modelagem/domain/Cidade.java
@@ -0,0 +1,47 @@
+package com.adriano.modelagem.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cidade implements Serializable {
+
+    private Integer id;
+    private String nome;
+
+    public Cidade() {
+    }
+
+    public Cidade(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cidade cidade = (Cidade) o;
+        return id.equals(cidade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ ModeloConceitual/src/main/java/com/adriano/modelagem/domain/Cliente.java
@@ -0,0 +1,99 @@
+package com.adriano.modelagem.domain;
+
+import com.adriano.modelagem.domain.enums.TipoCliente;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class Cliente implements Serializable {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String cpfCnpj;
+    private Integer tipoCliente;
+
+    private List<Endereco> enderecos = new ArrayList<>();
+    private Set<String> telefones = new HashSet<>();
+
+    public Cliente() {
+
+    }
+
+    public Cliente(Integer id, String nome, String email, String cpfCnpj, TipoCliente tipoCliente) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfCnpj = cpfCnpj;
+        this.tipoCliente = tipoCliente.getCodigo();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public TipoCliente getTipoCliente() {
+        return TipoCliente.toEnum(tipoCliente);
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente.getCodigo();
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Set<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(Set<String> telefones) {
+        this.telefones = telefones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return id.equals(cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
